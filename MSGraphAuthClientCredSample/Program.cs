139a5e4c80eb7e652805e1a5b357b39dfb309e93
Peter Wasonga Ombwa <pwombwa@live.com>
2019-06-11 11:56:01 -0700

    Add .gitignore.

---
+++ MSGraphAuthClientCredSample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+namespace MSGraphAuthClientCredSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+        public static async Task RunAsync()
+        {
+            try
+            {
+                var graphClient = GetGraphServiceClient();
+                var users = await graphClient.Users.Request().GetAsync();
+                var me = await graphClient.Users[users.FirstOrDefault().Id].Request().GetAsync();
+                Console.WriteLine($"me -> {me.DisplayName} - {me.Id}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static GraphServiceClient GetGraphServiceClient()
+        {
+            string clientId = "CLIENT_ID";
+            string clientSecret = "CLIENT_SECRET";
+            string tennatId = "TENNANT_ID";
+            var clientApplication = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithClientSecret(clientSecret)
+                .WithAuthority(new Uri($"https://login.microsoftonline.com/{tennatId}"))
+                .Build();
+
+            TokenStorageProvider.Initialize(clientApplication.AppTokenCache);
+
+            var authProvider = new ClientCredentialProvider(clientApplication);
+
+            return new GraphServiceClient(authProvider);
+        }
+    }
+}
---
+++ MSGraphAuthClientCredSample/TokenStorageProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Identity.Client;
+using System.Threading.Tasks;
+
+namespace MSGraphAuthClientCredSample
+{
+    public static class TokenStorageProvider
+    {
+        private static string TokenCacheFile = $"./tokencache.bin";
+        public static void Initialize(ITokenCache tokenCache)
+        {
+            tokenCache.SetBeforeAccessAsync(BeforeAccessAsync);
+            tokenCache.SetAfterAccessAsync(AfterAccessAsync);
+        }
+
+        private static async Task BeforeAccessAsync(TokenCacheNotificationArgs arg)
+        {
+            if (System.IO.File.Exists(TokenCacheFile))
+            {
+                byte[] tokenByte = await System.IO.File.ReadAllBytesAsync(TokenCacheFile);
+                // load token byte from storage to token cache.
+                arg.TokenCache.DeserializeMsalV3(tokenByte);
+            }
+        }
+
+        private static async Task AfterAccessAsync(TokenCacheNotificationArgs arg)
+        {
+            if (arg.HasStateChanged)
+            {
+                var tokenByte = arg.TokenCache.SerializeMsalV3();
+                // Write token cahce from cache to storage.
+                await System.IO.File.WriteAllBytesAsync(TokenCacheFile, tokenByte);
+            }
+        }
+    }
+}
